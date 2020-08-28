06069476abb0a5ea84927a0bf330f798d57ec9a0
Darren Kavanagh <dkav@users.noreply.github.com>
2020-08-28 11:53:33 -0700

    Update linkage_priority.py

--- linkage_priority.py
+++ linkage_priority.py
@@ -1,4 +1,6 @@
 import arcpy
+
+
 class ToolValidator(object):
   """Class for validating a tool's parameter values and controlling
   the behavior of the tool's dialog."""
@@ -6,9 +8,15 @@ class ToolValidator(object):
   def __init__(self):
     """Setup arcpy and the list of tool parameters."""
     self.params = arcpy.GetParameterInfo()
+
+    self.coretbl_param = self.params[11]
+    self.expertcoor_param = self.params[14]
     self.core_params = [12, 13, 14]
-    self.climate_params = range(17, 27)
-    self.wt_params = [28, 29, 30, 31]
+    self.climrast_param = self.params[15]
+    self.modadvance_param = self.params[16]
+    self.climate_params = range(17, 28)
+    self.expcoorwt_param = self.params[31]
+    self.climgradwt_param = self.params[32]
 
   def initializeParameters(self):
     """Refine the properties of a tool's parameters.  This method is
@@ -19,33 +27,28 @@ class ToolValidator(object):
     """Modify the values and properties of parameters before internal
     validation is performed.  This method is called whenever a parameter
     has been changed."""
-    if self.params[11].value:
+    if self.coretbl_param.value:
         for i in self.core_params:
             self.params[i].enabled = True
     else:
         for i in self.core_params:
             self.params[i].value = None
             self.params[i].enabled = False
-    
-    if self.params[14].value:
-	self.params[30].enabled = True
-    else:
-        self.params[30].enabled = False
 
-    if self.params[15].value:
-        self.params[16].enabled = True
-        self.params[31].enabled = True
-        #if self.wt_unaltered:
-        #   self.params[27].value = 0.25
-        #   self.params[28].value = 0.25
-        #   self.params[29].value = 0.25
-        #   self.params[31].value = 0.25
+    if self.expertcoor_param.value:
+        self.expcoorwt_param.enabled = True
     else:
-        self.params[16].value = False
-        self.params[16].enabled = False
-        self.params[31].enabled = False
+        self.expcoorwt_param.enabled = False
 
-    if self.params[16].value:
+    if self.climrast_param.value:
+        self.modadvance_param.enabled = True
+        self.climgradwt_param.enabled = True
+    else:
+        self.modadvance_param.value = False
+        self.modadvance_param.enabled = False
+        self.climgradwt_param.enabled = False
+
+    if self.modadvance_param.value:
         for i in self.climate_params:
             self.params[i].enabled = True
     else:
@@ -56,16 +59,7 @@ class ToolValidator(object):
   def updateMessages(self):
     """Modify the messages created by internal validation for each tool
     parameter.  This method is called after internal validation."""
-    if self.params[11].value is None:
+    if self.coretbl_param.value is None:
         for i in self.core_params:
             self.params[i].clearMessage()
     return
-
-  def wt_unaltered():
-    """Check if weights have been altered."""
-    unaltered = True
-    for param in self.wt_params:
-        if param.altered:
-	    unaltered = False
-	    break
-    return unaltered
